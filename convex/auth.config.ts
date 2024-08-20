be93ca8e47ce33413894d071314124d80778a011
Sanket Meshram <sanketmeshram41@gmail.com>
2024-08-20 23:07:40 +0530

    latest changes

---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://wanted-skunk-81.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+export default authConfig;
