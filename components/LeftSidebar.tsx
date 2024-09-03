657d7229f4124088d789f929d0a1cd2196d1e74a
Sanket Meshram <sanketmeshram41@gmail.com>
2024-09-03 13:09:54 +0530

    latest changes git latest changes

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -1,38 +1,51 @@
 "use client";
+
 import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
-import { SignedIn, SignedOut, useClerk, useUser } from "@clerk/nextjs";
+import { SignedIn, SignedOut, useClerk } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import React from "react";
 import { Button } from "./ui/button";
+import { useAudio } from "@/providers/AudioProvider";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
   const { signOut } = useClerk();
+  const { audio } = useAudio();
 
   return (
-    <section className="left_sidebar">
+    <section
+      className={cn("left_sidebar h-[calc(100vh-5px)]", {
+        "h-[calc(100vh-140px)]": audio?.audioUrl,
+      })}
+    >
       <nav className="flex flex-col gap-6">
-        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10">
+        <Link
+          href="/"
+          className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center"
+        >
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">
-            Podcasts
+            Podcastr
           </h1>
         </Link>
 
         {sidebarLinks.map(({ route, label, imgURL }) => {
           const isActive =
             pathname === route || pathname.startsWith(`${route}/`);
+
           return (
             <Link
               href={route}
               key={label}
               className={cn(
                 "flex gap-3 items-center py-4 max-lg:px-4 justify-center lg:justify-start",
-                { "bg-nav-focus border-r-4 border-orange-1": isActive }
+                {
+                  "bg-nav-focus border-r-4 border-orange-1": isActive,
+                }
               )}
             >
               <Image src={imgURL} alt={label} width={24} height={24} />
@@ -44,7 +57,7 @@ const LeftSidebar = () => {
       <SignedOut>
         <div className="flex-center w-full pb-14 max-lg:px-4 lg:pr-8">
           <Button asChild className="text-16 w-full bg-orange-1 font-extrabold">
-            <Link href="/sign-in">Sign In</Link>
+            <Link href="/sign-in">Sign in</Link>
           </Button>
         </div>
       </SignedOut>
